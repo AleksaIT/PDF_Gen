0ee7bf24c3a36376520abab2dda49f817ea16ddc
AleksaIT <aleksa.velickovic2@gmail.com>
2024-04-02 18:03:19 +0200

    Init commit

---
+++ .venv/main.py
@@ -0,0 +1,19 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",      #kad je w=0 onda border ide do kraja stranice,
+             ln=1)                                #ln=1 da ide u sledeci red
+    pdf.line(10, 22, 200, 22)
+
+
+pdf.output("output.pdf")
+
+pdf.add_page()
